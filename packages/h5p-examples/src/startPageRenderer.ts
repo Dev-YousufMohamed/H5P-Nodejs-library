5a1a4e8e1a615287916fa19cc18d722d59997968
Dev-YousufMohamed <141830411+Dev-YousufMohamed@users.noreply.github.com>
2023-10-29 07:11:28 +0200

    Update startPageRenderer.ts

--- packages/h5p-examples/src/startPageRenderer.ts
+++ packages/h5p-examples/src/startPageRenderer.ts
@@ -26,10 +26,10 @@ export default function render(
                 window.onload = function() {
                     let h5phref = '';
                     let backto = location.hash.replace('#','');
-                    let h5pWindow = open('https://testh5p.onrender.com/h5p/new', '', 'titlebar=no,toolbar=no,status=no,scrollbars=no,menubar=no,top=100,left=240,width=950,height=600');
+                    let h5pWindow = open('https://testinteractive.onrender.com/h5p/new', '', 'titlebar=no,toolbar=no,status=no,scrollbars=no,menubar=no,top=100,left=240,width=950,height=600');
                     h5pWindow.onload = function() {
                         let checkInterval = setInterval(function() {
-                            if (h5pWindow.location.href !== 'https://testh5p.onrender.com/h5p/new') {
+                            if (h5pWindow.location.href !== 'https://testinteractive.onrender.com/h5p/new') {
                                 h5phref = h5pWindow.location.href;
                                 clearInterval(checkInterval);
                                 h5pWindow.close();
